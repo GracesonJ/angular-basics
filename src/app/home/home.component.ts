1999034c3e8a99ee34fb0041660b2cd3d7e07abe
Graceson J <gracesonjjohnson@gmail.com>
2025-01-23 15:32:49 +0530

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgIf, NgFor, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -12,6 +14,10 @@ export class HomeComponent {
   imgUrl: string = "https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg"
   titleColor:string = "red"
   count:number = 0
+  username:string = ""
+  styleClass:string = "pStyle textColor"
+  userList:string[] = ["Alan", "Alex", "John", "Mark", "Jerin","Johny"]
+  UserAddress:string = "XYZ KL 652696"
 
   increment(){
     this.count++
@@ -19,5 +25,19 @@ export class HomeComponent {
 
   showAlert(data:string){
     alert(data)
+  }
+
+  getUsername(event:any){
+    console.log(event.target);
+    this.username = event.target.value
+    
+  }
+
+  getUser(tag:any){
+    if(tag.value){
+      alert(`Designation : ${tag.value}`)
+    }else{
+      alert(`Fill the Form`)
+    }
   }
 }
